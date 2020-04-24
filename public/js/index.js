dcafe5bb45153df0279e417984d008d3e03e376b
La-Guillotine <58303108+La-Guillotine@users.noreply.github.com>
2020-04-24 17:58:33 +0200

    recupération de l'objet plante en js lors de sa sélection

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $("select").select2();
+    $(".listPlantes > select").on('change', function(){
+        let option = this.options[this.selectedIndex];
+        console.log(option);
+        let plante = $(option).data();
+        console.log(plante);
+        alert(plante.plante.nom);
+    });
 });
+
